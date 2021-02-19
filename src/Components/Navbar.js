e43b776866b3b60a6a6253520264809f3e0e35f0
Fikri Akmal <fikriakmal453@gmail.com>
2021-02-19 19:33:06 +0700

    test deploy

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         textDecoration:'none'
     }
 
-    const content = ["about", "background", "skills", "experiences", "contact"]
+    const content = ["about", "background", "skills", "experiences", "contact", "projects"]
     
     return (
         <>
